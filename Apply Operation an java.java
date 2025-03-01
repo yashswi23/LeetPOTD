512bb1f5836ef2f8e944f9f0265592ef88e9758c
Yashswi sharma <142489383+yashswi23@users.noreply.github.com>
2025-03-01 12:50:02 +0530

    Aply Operation on array in java Sol

---
+++ Apply Operation an java.java
@@ -0,0 +1,20 @@
+public int[] applyOperations(int[] nums) {
+        int n  = nums.length;
+        for(int i=0;i<n-1;i++){
+            if(nums[i] == nums[i+1]){
+                nums[i] = nums[i]*2;
+                nums[i+1] = 0;
+            }
+        }
+    int[]temp = new int[n];
+    int index=0;
+       for(int num:nums){
+        if(num!=0){
+            temp[index] = num;
+            index++;
+        }
+       }
+
+
+        return temp;
+    }
